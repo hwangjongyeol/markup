03afa5cad252d2425a6146328865ba53aff0d367
황종열 젬백스링크 <jongyeol@flk.kr>
2024-03-19 15:47:01 +0900

    mail test

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -61,7 +61,7 @@
             return false;
         }
 
-        disableAllButtons(form);
+        //disableAllButtons(form);
         var url = form.action;
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url);
@@ -102,4 +102,18 @@
             buttons[i].disabled = true;
         }
     }
+    function check() {
+        var email = document.getElementById("email").value;
+        if (email != "") {
+            var exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+            if (exptext.test(email) == false) {
+                //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우
+                alert("입력한 메일형식이 올바르지 않습니다.");
+                document.formtag.email.focus();
+                return false;
+            }
+        }
+        return true;
+    }
+
 })();
